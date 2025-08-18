42634ec3f90baba4741f1597d5d1dae979395d86
Ksenia Chekel <chekel@dev.vtb.ru>
2025-08-18 19:37:21 +0300

    feature/security add exceptions

---
+++ src/main/java/eu/senla/network/exceptions/AuthException.java
@@ -0,0 +1,7 @@
+package eu.senla.network.exceptions;
+
+public class AuthException extends RuntimeException{
+    public AuthException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/eu/senla/network/exceptions/DuplicateUserException.java
@@ -0,0 +1,7 @@
+package eu.senla.network.exceptions;
+
+public class DuplicateUserException extends RuntimeException{
+    public DuplicateUserException(String message) {
+        super(message);
+    }
+}
